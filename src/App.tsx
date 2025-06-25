8e40b246a60a89aa4200a2e1098290d38f137fb3
th <thiagorerezende@gmail.com>
2025-06-25 17:06:59 -0300

    todo feito

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import {Item} from './types/item'
+import * as C from './App.styles';
+import { ListItem } from './components/listItem'
+import { AddArea } from './components/AddArea';
+const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'sua mae', done: false},
+    {id: 2, name: 'seu pai', done: true}
+  ])
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list]
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    })
+    setList(newList)
+  }
+
+  return(
+    <C.Container>
+      <C.Area>
+        <C.Header>Olá</C.Header>
+
+        <AddArea onEnter={handleAddTask} />
+
+        {list.map(((item, index) => 
+          <ListItem 
+          key={index}
+          item={item}
+          />
+        ))}
+      </C.Area>
+    </C.Container>
   );
+
 }
 
-export default App;
+export default App
---
+++ src/components/listItem/index.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { Item } from '../../types/item'
+import * as S from './styles'
+
+type Props = {
+    item: Item
+}
+
+export const  ListItem = ({item}: Props) => {
+
+    const [isChecked, setIsChecked] = useState(item.done)
+
+    return (
+        <S.Container done={isChecked}>
+            <input 
+                type='checkbox' 
+                checked={isChecked}
+                onChange={e => setIsChecked(e.target.checked)}
+            />
+            <label>{item.name}</label>
+        </S.Container>
+    )
+}
